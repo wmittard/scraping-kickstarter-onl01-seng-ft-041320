3ea905cdaddff3133fdb2729025dce80905b5300
William Mittard <billymittard@ymail.com>
2020-04-30 20:46:20 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,7 +17,13 @@ kickstarter = Nokogiri::HTML(html)
 
 projects = {}
 
-kickstarter.css("li")
+kickstarter.css("li.project.grid_4").each do |project| 
+  projects[project]
+  = {}
+end 
+
+projects 
+end 
 end
 
 
