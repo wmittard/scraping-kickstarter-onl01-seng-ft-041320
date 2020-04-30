173076bec4cf196d97b9236048a0a77289dda192
William Mittard <billymittard@ymail.com>
2020-04-30 20:42:11 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,7 @@ html = File.read('fixtures/kickstarter.html')
 #title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+#
 kickstarter = Nokogiri::HTML(html)
 binding.pry
 end
